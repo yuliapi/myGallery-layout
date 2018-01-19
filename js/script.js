d4b1e7a80d0b45d9a08c67e81a7bd0fd658da3de
yulia <zEEcNCTDud0l>
2018-01-19 13:40:03 -0800

    code refactoring

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function firstByClass(parent, selector) {
 class DisplayUtils {
     static createClasses(n, mod) {
         let t;
-        if (n < 15) {
+        if (n < itemsInPeriod) {
             t = "gallery-item item" + n;
         }
         if (mod === 0) {
@@ -27,36 +27,49 @@ class DisplayUtils {
         else {
             t = "gallery-item item" + mod;
         }
+        //to display always vertical (except viewport < 800px)
         if (this.isVertical(mod, n)) {
             t = t + " vertical"
         }
+        //800 < viewport < 1200 display vertical
         if (this.isSmallVertical(mod)) {
             t = t + " vertical-normal push-left"
         }
+        //this rule applies to 5th, 11th, and 12th element.
+        //always horizontal
+        // and 800 < viewport < 1200 pushed-right
         if (mod === 5 || mod === 11 || mod === 12) {
             t = t + " push-right"
         }
+        //800 < viewport < 1200 display horizontal and push right
+        //viewport >= 1200 display vertical
         if (this.isLargeVertical(mod)) {
             t = t + " normal-vertical  push-right"
         }
+
+        //800 < viewport < 1200 display vertical
         if (this.isNinthItem(mod)) {
             t = t + " vertical-normal"
         }
         return t
     };
 
+    //select every 3th and 15th
     static isVertical(mod, n) {
-        return mod === 3 || n === 15 || (n - 15) / 15 % 1 === 0;
+        return mod === 3 || n === itemsInPeriod || (n - itemsInPeriod) / itemsInPeriod % 1 === 0;
     }
 
+    //select every 6th item
     static isLargeVertical(mod) {
         return mod === 6;
     }
 
+    //select every 4th and 10th item
     static isSmallVertical(mod) {
         return (mod === 4 || mod === 10)
     }
 
+    // select every 9th item
     static isNinthItem(mod) {
         return mod === 9
     }
@@ -71,76 +84,68 @@ class DisplayUtils {
         }
         return node
     }
+
     static isViewportSmall() {
         return viewport > smallBreak && viewport < largeBreak
     }
+
     static isViewportLarge() {
         return viewport >= largeBreak
     }
-
 }
-
 class ServerEntry {
-    constructor(id) {
-        this.id = id;
-        this.text = '#' + id;
+    constructor(n) {
+        this.counter = n;
+        this.text = 'Here will be the text for #' + (this.counter + 1);
     }
 
     getJson() {
-        return {"id": this.id, "text": [this.text]};
+        return {"counter": this.counter, "text": [this.text]};
     };
 }
-
 class DisplayEntry {
     constructor(entry, totalEntriesCount) {
         this.entry = entry;
-        // this.id = entry.id;
-        // this.text = entry.text;
+        this.entryNumber = this.entry.counter + 1;
         this.totalEntriesCount = totalEntriesCount;
+        this.modulus = this.entryNumber % itemsInPeriod; // is used to determine every 1, 2, 3 ... 15 gallery-item
         this.template = document.getElementById('galleryItemInner').innerHTML;
-        // this.photo = entry.photo;
-        // this.photoWidth = entry.photoWidth;
-        // this.photoHeight = entry.photoHeight;
-        // this.number = this.entry.id + 1;
-        this.title = entry.title
     }
 
     display(generalContainer, lastRowContainer) {
-        let totalMod = 0;
+        let totalModulus = 0;
         let needExtraRow = false;
         if (DisplayUtils.isViewportSmall()) {
-            totalMod = (this.totalEntriesCount % itemsInPeriod) % smallScreenRowCount;
-            if (totalMod < smallScreenRowCount) {
+            totalModulus = (this.totalEntriesCount % itemsInPeriod) % smallScreenRowCount;
+            if (totalModulus < smallScreenRowCount) {
                 needExtraRow = true;
             }
         }
         if (DisplayUtils.isViewportLarge()) {
-            totalMod = (this.totalEntriesCount % itemsInPeriod) % largeScreenRowCount;
-            if (totalMod < largeScreenRowCount - 1) { /*on large screen with max 5 items in a row  only 4 filled is ok and don't require last row*/
+            totalModulus = (this.totalEntriesCount % itemsInPeriod) % largeScreenRowCount;
+            if (totalModulus < largeScreenRowCount - 1) { /*on large screen with max 5 items in a row  only 4 filled is ok and don't require last row*/
                 needExtraRow = true;
             }
         }
-
-        let num = this.entry.id + 1;
-        let mod = num % itemsInPeriod;
-
-        let box = DisplayUtils.createNode('div', "box", num);
-        if (needExtraRow && this.entry.id >= this.totalEntriesCount - totalMod) {
-            if (generalContainer.clientWidth) {
+        let box = DisplayUtils.createNode('div', "box", this.entryNumber);
+        if (needExtraRow && this.entry.counter >= this.totalEntriesCount - totalModulus) {
+            if (generalContainer.clientWidth && DisplayUtils.isViewportLarge()) {
                 lastRowContainer.style.width = generalContainer.clientWidth + "px";
+            } else if(DisplayUtils.isViewportSmall()) {
+                lastRowContainer.style.width = '640px';
             }
             let wrapper = DisplayUtils.createNode('div', "gallery-item");
-            this.addInner(wrapper, mod, num);
+            this.addInner(wrapper, this.modulus, this.entryNumber);
             lastRowContainer.appendChild(wrapper);
         } else {
-            let classes = DisplayUtils.createClasses(num, mod);
+            let classes = DisplayUtils.createClasses(this.entryNumber, this.modulus);
             let wrapper = DisplayUtils.createNode('div', classes);
-            this.addInner(wrapper, mod, num);
+            this.addInner(wrapper, this.modulus, this.entryNumber);
             generalContainer.appendChild(wrapper);
         }
     }
 
-    addInner(parent, mod, num) {
+    addInner(parent) {
         parent.innerHTML = this.template;
         let text = firstByClass(parent, 'entry-text');
         if (this.entry.text && this.entry.text.length > 0) {
@@ -150,19 +155,16 @@ class DisplayEntry {
             })
         }
         let header = firstByClass(parent, 'box-header');
-        let hNumber = DisplayUtils.createNode('h4', 'number', '<span>&#8470</span>' + ++this.entry.id);
+        let hNumber = DisplayUtils.createNode('h4', 'number', '<span>&#8470</span>' + this.entryNumber);
         header.appendChild(hNumber);
         if (this.entry.title) {
             let hText = DisplayUtils.createNode('h4', 'title', this.entry.title);
             header.appendChild(hText);
         }
-
         if (this.entry.photo && this.entry.photo.length > 0) {
-            if (DisplayUtils.isVertical(mod) || DisplayUtils.isLargeVertical(mod) && DisplayUtils.isViewportLarge()
-                || (DisplayUtils.isNinthItem(mod) || DisplayUtils.isSmallVertical(mod)) && DisplayUtils.isViewportSmall()) {
-                this.entry.photo = '<a href="https://placeholder.com"><img src="http://via.placeholder.com/216x216"></a>'
+            if (this.isNeedToAdjust()) {
+                this.adjust()
             }
-
             let div = firstByClass(parent, 'box-content');
             let photo = firstByClass(parent, 'entry-photo');
             div.classList.add("illustrated");
@@ -173,14 +175,25 @@ class DisplayEntry {
             if (this.entry.photoWidth > this.entry.photoHeight) {
                 text.classList.add('landscape');
                 photoholder.classList.add('landscape');
-            } else {
+            } else if (this.entry.photoWidth < this.entry.photoHeight) {
                 text.classList.add('portrait');
                 photoholder.classList.add('portrait');
             }
         }
     }
-}
 
+    isNeedToAdjust() {
+        if (DisplayUtils.isVertical(this.modulus) || DisplayUtils.isLargeVertical(this.modulus) && DisplayUtils.isViewportLarge()
+            || (DisplayUtils.isNinthItem(this.modulus) || DisplayUtils.isSmallVertical(this.modulus)) && DisplayUtils.isViewportSmall()) {
+            return true
+        }
+    }
+
+    adjust() {
+        this.entry.photo = '<a href="https://placeholder.com"><img src="http://via.placeholder.com/216x216"></a>'
+        this.entry.photoWidth = this.entry.photoHeight = 216
+    }
+}
 
 function addContent() {
     document.getElementById("myGallery").innerHTML = "";
@@ -190,7 +203,7 @@ function addContent() {
     let lastRowContainer = document.getElementById("gallery-last-row");
     let entries = [
         {
-            'id': 0,
+            'counter': 0,
             'photo': '<a href="https://placeholder.com"><img src="http://via.placeholder.com/153x230"></a>',
             'photoWidth': 153,
             'photoHeight': 230,
@@ -198,7 +211,7 @@ function addContent() {
             "text": ['Legimus noluisse dissentiet ea qui. Ex his placerat oporteat! Noluisse invidunt ei sed, nam eilaoreet iudicabit adipiscing, odio perfecto te mel!']
         },
         {
-            'id': 1,
+            'counter': 1,
             'photo': '<a href="https://placeholder.com"><img src="http://via.placeholder.com/315x150"></a>',
             'photoWidth': 315,
             'photoHeight': 150,
@@ -206,49 +219,46 @@ function addContent() {
             "text": ['Legimus noluisse dissentiet ea qui. Ex his placerat oporteat! Noluisse invidunt ei sed, nam eilaoreet iudicabit adipiscing, odio perfecto te mel!']
         },
         {
-            'id': 2,
+            'counter': 2,
+            'photo': '<a href="https://placeholder.com"><img src="http://via.placeholder.com/153x230"></a>',
+            'photoWidth': 153,
+            'photoHeight': 230,
+            'title': "Erat nemore propriae no",
+            "text": ['Legimus noluisse dissentiet ea qui. Ex his placerat oporteat! Noluisse invidunt ei sed, nam eilaoreet iudicabit adipiscing, odio perfecto te mel!']
+        },
+        {
+            'counter': 3,
+            'photo': '<a href="https://placeholder.com"><img src="http://via.placeholder.com/315x150"></a>',
+            'photoWidth': 315,
+            'photoHeight': 150,
+            'title': "Erat nemore propriae no",
+            "text": ['Legimus noluisse dissentiet ea qui. Ex his placerat oporteat! Noluisse invidunt ei sed, nam eilaoreet iudicabit adipiscing, odio perfecto te mel!']
+        },
+        {
+            'counter': 4,
+            'photo': '<a href="https://placeholder.com"><img src="http://via.placeholder.com/153x230"></a>',
+            'photoWidth': 153,
+            'photoHeight': 230,
+
+            'title': "Erat nemore propriae no",
+            "text": ['Legimus noluisse dissentiet ea qui. Ex his placerat oporteat! Noluisse invidunt ei sed, nam eilaoreet iudicabit adipiscing, odio perfecto te mel!']
+        },
+
+        {
+            'counter': 5,
             'photo': '',
             'title': "Erat nemore propriae no",
             "text": ['Has eu debitis delicata deterruisset. Nec veniam audiam ut. Utroque commune accumsan eihis, in mei mandamus recteque petentium! Mundi nemore an vel, persius veritus perfectovis te. Enim doming ius cu.',
                 'Sea ponderum suavitate consequuntur ex, has latine aperiam neglegentur ne. Ei iriure principes consulatu est, tollit suscipit mea at, eu alii constituam eam! Eruditiefficiendi cu eam, alterum mentitum salutatus an per, eu sed facer labores. Vis dicamaperiam bonorum an, qui luptatum ocurreret id, nibh harum ancillae sea eu. Nec id proboomnesque, apeirian erroribus signiferumque sea te.']
         },
-        {
-            'id': 3,
-            'photo': '<a href="https://placeholder.com"><img src="http://via.placeholder.com/315x150"></a>',
-            'photoWidth': 315,
-            'photoHeight': 150,
-            'title': "Erat nemore propriae no",
-            "text": ['Legimus noluisse dissentiet ea qui. Ex his placerat oporteat! Noluisse invidunt ei sed, nam eilaoreet iudicabit adipiscing, odio perfecto te mel!']
-        },
-        {
-            'id': 4,
-            'photo': '<a href="https://placeholder.com"><img src="http://via.placeholder.com/153x230"></a>',
-            'photoWidth': 153,
-            'photoHeight': 230,
-
-            'title': "Erat nemore propriae no",
-            "text": ['Legimus noluisse dissentiet ea qui. Ex his placerat oporteat! Noluisse invidunt ei sed, nam eilaoreet iudicabit adipiscing, odio perfecto te mel!']
-        },
-        {
-            'id': 5,
-            'photo': '<a href="https://placeholder.com"><img src="http://via.placeholder.com/153x230"></a>',
-            'photoWidth': 153,
-            'photoHeight': 230,
-            'title': "Erat nemore propriae no",
-            "text": ['Legimus noluisse dissentiet ea qui. Ex his placerat oporteat! Noluisse invidunt ei sed, nam eilaoreet iudicabit adipiscing, odio perfecto te mel!']
-        }
-
     ];
     for (let i = 6; i <= totalEntries - 1; i++) {
         let entry = new ServerEntry(i);
         entries.push(entry.getJson());
     }
-
     for (let entry of entries) {
         let displayEntry = new DisplayEntry(entry, entries.length);
         displayEntry.display(generalContainer, lastRowContainer);
 
     }
-
-
 }
